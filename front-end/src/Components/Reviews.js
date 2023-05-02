b5832c2761371d51cd277e30125e3523060e0e86
SuperNinjaEv <evan0210@gmail.com>
2023-05-01 22:30:11 -0400

    Working on solving review issue, not showing up, but no errors

--- front-end/src/Components/Reviews.js
+++ front-end/src/Components/Reviews.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import Review from "./Review";
+import Review from "./Review"
 import ReviewForm from "./ReviewForm";
 
 const API = process.env.REACT_APP_API_URL;
